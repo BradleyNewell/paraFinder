45b3856003dc12ef6c1ecc683d40075add0de971
Bradley <40966735+BradleyXVIII@users.noreply.github.com>
2019-07-25 00:22:13 +0100

    Add files via upload

---
+++ parasentFinder/pFinder.py
@@ -0,0 +1,34 @@
+import re
+
+file = input("Enter file path:")
+term = input("Enter search term:")
+search_selection = input('Please enter "Sentence" to search for sentences including your search term or'
+                         ' "Paragraph" to search for paragraphs including your search term: ')
+
+
+def getText():
+    with open(file, 'r', encoding='utf-8') as text:
+        text = text.read()
+        keyword = term
+        try:
+            if search_selection.title() == "Sentence":
+                sentence = re.findall(r'([^.]*' + keyword + '[^.]*)', text)
+                str(sentence)
+                for line in sentence:
+                    line = line.replace("\n", ' ')
+                    print(line)
+
+            elif search_selection.title() == "Paragraph":
+                paragraph = re.findall(r'([^\n]*' + keyword + '[^\n]*)', text)
+
+                print(paragraph)
+            else:
+                print("Please enter a valid search selection")
+        except NameError:
+            print("Enter valid name")
+
+
+getText()
+
+
+
