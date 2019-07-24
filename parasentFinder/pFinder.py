7a2357a8c1f710e265c062d481b242937f6705ff
Bradley <40966735+BradleyXVIII@users.noreply.github.com>
2019-07-25 00:44:15 +0100

    Update pFinder.py

--- parasentFinder/pFinder.py
+++ parasentFinder/pFinder.py
@@ -1,30 +1,41 @@
 import re
 
 file = input("Enter file path:")
+
 file = file + '.txt'
+
 term = input("Enter search term:")
+
 search_selection = input('Please enter "Sentence" to search for sentences including your search term or'
                          ' "Paragraph" to search for paragraphs including your search term: ')
 
 
 def getText():
+  
     with open(file, 'r', encoding='utf-8') as text:
+        
         text = text.read()
         keyword = term
+        
         try:
             if search_selection.title() == "Sentence":
                 sentence = re.findall(r'([^.]*' + keyword + '[^.]*)', text)
                 str(sentence)
+                if keyword not in sentence:
+                    print("No results found")
                 for line in sentence:
                     line = line.replace("\n", ' ')
                     print(line)
 
             elif search_selection.title() == "Paragraph":
                 paragraph = re.findall(r'([^\n]*' + keyword + '[^\n]*)', text)
-
+                if keyword not in paragraph:
+                    print("No results found")
                 print(paragraph)
+                
             else:
                 print("Please enter a valid search selection")
+                
         except NameError:
             print("Enter valid name")
 
